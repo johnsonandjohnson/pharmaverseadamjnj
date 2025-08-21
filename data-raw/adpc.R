34883ac14b08b7adf63e3a57c1b42e922b1ae885
RMao6 <rmao6@its.jnj.com>
2025-08-21 14:48:23 +0000

    Undo drug name replacement

--- data-raw/adpc.R
+++ data-raw/adpc.R
@@ -15,57 +15,7 @@ gen_adpc <- function(seed = 123) {
 
   # Get source data
   raw <- pharmaverseadam::adpc
-
-  gen <- dplyr::mutate(
-    raw,
-    PCTESTCD = as.factor(dplyr::case_match(
-      PCTESTCD,
-      "XAN" ~ "APA",
-      .default = PCTESTCD
-    )),
-    PCTEST = as.factor(dplyr::case_match(
-      PCTEST,
-      "XANOMELINE" ~ "APALUTAMIDE",
-      .default = PCTEST
-    )),
-    TRT01P = forcats::fct_recode(
-      TRT01P,
-      "Apalutamide" = "Xanomeline High Dose",
-      "Apalutamide Subgroup" = "Xanomeline Low Dose"
-    ),
-    TRT01A = forcats::fct_recode(
-      TRT01A,
-      "Apalutamide" = "Xanomeline High Dose",
-      "Apalutamide Subgroup" = "Xanomeline Low Dose"
-    ),
-    PARAMCD = as.factor(dplyr::case_match(
-      PARAMCD,
-      "XAN" ~ "APA",
-      .default = PARAMCD
-    )),
-    PARAM = as.factor(gsub("Xanomeline", "Apalutamide", PARAM)),
-    ARMCD = as.factor(dplyr::case_when(
-      ARMCD == "Xan_Hi" ~ "Apa",
-      ARMCD == "Xan_Lo" ~ "Apa_Sub",
-      .default = ARMCD
-    )),
-    ARM = as.factor(dplyr::case_when(
-      ARM == "Xanomeline High Dose" ~ "Apalutamide",
-      ARM == "Xanomeline Low Dose" ~ "Apalutamide Subgroup",
-      .default = ARM
-    )),
-    ACTARMCD = as.factor(dplyr::case_when(
-      ACTARMCD == "Xan_Hi" ~ "Apa",
-      ACTARMCD == "Xan_Lo" ~ "Apa_Sub",
-      .default = ACTARMCD
-    )),
-    ACTARM = as.factor(dplyr::case_when(
-      ACTARM == "Xanomeline High Dose" ~ "Apalutamide",
-      ACTARM == "Xanomeline Low Dose" ~ "Apalutamide Subgroup",
-      .default = ACTARM
-    ))
-  )
-
+  
   # Handle NA values and convert characters to factors
   gen <- df_na(gen, char_as_factor = TRUE)
 
