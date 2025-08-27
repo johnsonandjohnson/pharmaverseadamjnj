021d3a5dddf3d3cea962c310979af9d4a0606bee
RMao6 <rmao6@its.jnj.com>
2025-08-27 09:25:50 +0000

    Derive `HYPSCAT`

--- data-raw/adagocmq.R
+++ data-raw/adagocmq.R
@@ -9,8 +9,16 @@ source(file.path("data-raw", "helpers.R"))
 # For `HYPSCAT` derivation. Downloaded from:
 # https://www.fda.gov/drugs/development-resources/
 # office-new-drugs-custom-medical-queries-ocmqs
-terms <- file.path("data-raw", "OCMQs_v3.0.xlsm") |>
-  readxl::read_excel(sheet = "Hypersensitivity")
+terms <-
+  file.path("data-raw", "OCMQs_v3.0.xlsm") |>
+  readxl::read_excel(sheet = "Hypersensitivity") |>
+  
+  # For ease of operation
+  dplyr::mutate(
+    AEDECOD = toupper(Term),
+    HYPSCAT = substr(`Algorithmic Category`, 1, 1)
+  ) |>
+  dplyr::select(AEDECOD, HYPSCAT)
 
 # Generate ADAGOCMQ dataset
 gen_adagocmq <- function(seed = 123) {
@@ -21,9 +29,14 @@ gen_adagocmq <- function(seed = 123) {
   raw <- adaeocmq
   
   # Main logic
+  gen <- raw |> 
+    # Derive `HYPSCAT`
+    dplyr::left_join(terms)
   
   # Define additional labels for new variables not in source dataset
-  additional_labels <- list()
+  additional_labels <- list(
+    HYPSCAT = "Hypersensitivity Category"
+  )
   
   # Handle NA values and convert characters to factors
   gen <- df_na(gen, char_as_factor = TRUE)
