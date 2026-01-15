e9f4614314a7c23e01cb51dac467089186aeacb4
RMao6 <rmao6@its.jnj.com>
2026-01-15 09:34:16 +0000

    Add `DCTADY`

--- data-raw/adsl.R
+++ data-raw/adsl.R
@@ -392,6 +392,11 @@ gen_adsl <- function(seed = 123) {
     PKFL = dplyr::if_else(USUBJID %in% pharmaverseadam::adpc$USUBJID, "Y", "N"),
     DIABETFL = sample(c("N", "Y"), NROW(gen), TRUE, c(0.8, 0.2))
   )
+  
+  gen <- dplyr::mutate(
+    gen,
+    DCTADY = DCTDT - TRTSDT + 1
+  )
 
   # Define additional labels for new variables not in source dataset
   additional_labels <- list(
@@ -451,7 +456,8 @@ gen_adsl <- function(seed = 123) {
     RESCRNFL = "Re-screened Flag",
     ITTFL = "Intent-To-Treat Population Flag",
     PKFL = "Pharmacokinetic Population Flag",
-    DIABETFL = "History of Diabetes"
+    DIABETFL = "History of Diabetes",
+    DCTADY = " Day of Discontinuation of Study Treatment"
   )
 
   # Handle NA values and convert characters to factors
