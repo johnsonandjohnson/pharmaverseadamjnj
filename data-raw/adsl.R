00537da340aaff246202a311192ad34ac5ee2794
munoztd0 <david.munoztord@mailbox.org>
2026-05-08 10:51:39 +0200

    update : add back _DECODE

--- data-raw/adsl.R
+++ data-raw/adsl.R
@@ -144,6 +144,48 @@ gen_adsl <- function(seed = 123) {
     gen$BMIBLG1N
   )
   gen$SEX <- as.factor(gen$SEX)
+  gen$COUNTRY_DECODE <- as.factor("United States of America")
+
+  gen$RACE_DECODE <- factor(
+    dplyr::case_when(
+      gen$RACE == "AMERICAN INDIAN OR ALASKA NATIVE" ~
+        "American Indian or Alaska Native",
+      gen$RACE == "ASIAN" ~ "Asian",
+      gen$RACE == "BLACK OR AFRICAN AMERICAN" ~ "Black or African American",
+      gen$RACE == "NATIVE HAWAIIAN OR OTHER PACIFIC ISLANDER" ~
+        "Native Hawaiian or other Pacific Islander",
+      gen$RACE == "WHITE" ~ "White",
+      gen$RACE == "MULTIPLE" ~ "Multiple",
+      gen$RACE == "NOT REPORTED" ~ "Not reported",
+      gen$RACE == "UNKNOWN" ~ "Unknown",
+      gen$RACE == "OTHER" ~ "Other"
+    ),
+    levels = c(
+      "American Indian or Alaska Native",
+      "Asian",
+      "Black or African American",
+      "Native Hawaiian or other Pacific Islander",
+      "White",
+      "Multiple",
+      "Not reported",
+      "Unknown",
+      "Other"
+    )
+  )
+
+   gen$SEX_DECODE <- factor(
+    dplyr::case_when(
+      gen$SEX == "F" ~ "Female",
+      gen$SEX == "M" ~ "Male"
+    ),
+    levels = c(
+      "Male",
+      "Female",
+      "Intersex",
+      "Unknown"
+    )
+  )
+
   gen$REGION1 <- "North America"
   gen$RACEGR1 <- as.factor(gen$RACEGR1)
   gen$RFICDTC <- gen$DMDTC
@@ -472,7 +514,11 @@ gen_adsl <- function(seed = 123) {
     FASEXRS = "Reason for Excl from Full Analysis Set",
     PPREXRS = "Reason for Excl from Per-Prot Population",
     PKEXRES = "Reason for Excl from Pharmacokin Pop",
-    IMEXRES = "Reason for Excl from Immunogen Pop"
+    IMEXRES = "Reason for Excl from Immunogen Pop",
+    SEX_DECODE = "Sex Decode",
+    RACE_DECODE = "Race Decode",
+    ETHNIC_DECODE = "Ethnicity Decode",
+    COUNTRY_DECODE = "Country Decode"
   )
 
   # Handle NA values and convert characters to factors
