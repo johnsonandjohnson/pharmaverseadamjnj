97a96044b153cb74b10566bd3b0be7a3948b4dca
Vikram Rawat <vickey.coool@gmail.com>
2026-06-11 12:16:43 +0000

    created dummy data for paramcd for now

--- data-raw/adishum.R
+++ data-raw/adishum.R
@@ -27,6 +27,67 @@ gen_adishum <- function(seed = 123) {
       USUBJID
     )
 
+  sdtm_tbl <- pharmaversesdtm::is_ada |> 
+    left_join(
+      pharmaverseadamjnj::adsl,
+      by = "USUBJID"
+    ) |> 
+    select(
+      IMFL, 
+      SAFFL,
+      ISBDAGNT,
+      TRTSDT, 
+      TRTSDTM,
+      TRT01P, 
+      TRT01PN, 
+      TRT01A, 
+      TRT01AN, 
+      AGE, 
+      SEX, 
+      RACE, 
+      SITEID, 
+      SUBJID
+    )
+  
+  sdtm_tbl <- sdtm_tbl |> 
+    mutate(
+      PARQUAL = if_else(
+        is.na(ISBDAGNT), 
+        NA_character_,
+        ISBDAGNT |> 
+          str_trim() |> 
+          str_to_upper()
+    )
+  )
+
+   # tribble of PARAMCD, PARAM, ISTESTCD
+  temp_paramcd <- tibble::tribble(
+    ~PARAMCD,  ~PARAM,                                                      ~ISTESTCD,
+    "ADABL",    "Binding ADA, Last Result",                                 "ADA_BAB",
+    "ADABLT",   "Binding ADA, Titer",                                       "ADA_BAB",
+    "ADATRB",   "Treatment-Emergent ADA, Result",                           "ADA_BAB",
+    "ADATRBT",  "Treatment-Emergent ADA, Titer",                            "ADA_BAB",
+    "ADANTRB",  "Neutralizing ADA, Result",                                 "ADA_NAB",
+    "ADANTRBT", "Neutralizing ADA, Titer",                                  "ADA_NAB",
+    "ADATRI",   "Treatment-Induced ADA, Result",                            "ADA_BAB",
+    "ADATRIPT", "Treatment-Induced ADA, Titer",                             "ADA_BAB",
+    "ADATRE",   "Treatment-Enhanced ADA, Result",                           "ADA_BAB",
+    "ADATREPT", "Treatment-Enhanced ADA, Titer",                            "ADA_BAB",
+    "ADATREPT", "Treatment-Enhanced ADA, Titer (duplicate entry)",          "ADA_BAB",
+    "ADANTRE",  "Treatment-Enhanced Neutralizing ADA, Result",              "ADA_NAB",
+    "ADAPSP",   "ADA Persistent Positive, Result",                          "ADA_BAB",
+    "ADATSP",   "Neutralizing ADA Persistent Positive",                     "ADA_NAB",
+    "ADAUND",   "ADA Undetermined, Result",                                 "ADA_BAB",
+    "TMOSADAW", "Time to Onset of ADA (weeks)",                             NA_character_,
+    "ADADURW",  "Duration of ADA Response (weeks)",                         NA_character_,
+    "PSPDURW",  "Duration of Persistent Positive ADA (weeks)",              NA_character_,
+    "NABPOS",   "Neutralizing ADA Positive",                                "ADA_NAB",
+    "NABNEG",   "Neutralizing ADA Negative",                                "ADA_NAB"
+  )
+
+
+
+
 
 
 
