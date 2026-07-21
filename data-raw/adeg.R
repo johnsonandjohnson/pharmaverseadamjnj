3828e4be580834fde09d46d38d820846eff6d532
PArunZaw <PArunZaw@ITS.JNJ.com>
2026-07-21 07:14:08 +0000

    data-raw/adeg.R, data/adeg.rda, inst/extdata/adeg.xpt

--- data-raw/adeg.R
+++ data-raw/adeg.R
@@ -145,11 +145,11 @@ gen_adeg <- function(seed = 123) {
   # and set EGCLSIG accordingly
   intp_rows <- which(gen$PARAMCD == "INTP")
   # Randomly assign NORMAL or ABNORMAL to AVALC for INTP rows
-  gen$AVALC[intp_rows] <- sample(c("NORMAL", "ABNORMAL"), length(intp_rows), replace = TRUE, prob = c(0.5, 0.5))
+  gen$AVALC[intp_rows] <- sample(c("ABNORMAL, CS", "ABNORMAL, NCS", "NORMAL"), length(intp_rows), replace = TRUE, prob = c(0.5, 0.5, 0.5))
 
   # Set EGCLSIG based on AVALC
   normal_rows <- intp_rows[gen$AVALC[intp_rows] == "NORMAL"]
-  abnormal_rows <- intp_rows[gen$AVALC[intp_rows] == "ABNORMAL"]
+  abnormal_rows <- intp_rows[gen$AVALC[intp_rows] == "ABNORMAL, CS"]
 
   # Set EGCLSIG to NA for NORMAL rows
   gen$EGCLSIG[normal_rows] <- NA
