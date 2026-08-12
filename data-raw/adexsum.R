b00f6eb5a911990f6226da41a2616b6f08147c70
David Muñoz Tord <david.munoz@mailbox.org>
2026-08-12 15:34:31 +0200

    Update data-raw/adexsum.R

--- data-raw/adexsum.R
+++ data-raw/adexsum.R
@@ -87,7 +87,7 @@ gen_adexsum <- function(seed = 123) {
   gen <- dplyr::bind_rows(records)
 
   # Adding Duration of treatment days and years
-  trtdur <- gen %>%
+  trtdur <- gen |>
     dplyr::filter(PARAMCD == "TRTDURM") %>%
     dplyr::bind_rows(
       dplyr::mutate(., PARAM = "Duration of treatment (days)", PARAMCD = "TRTDURD", AVAL = AVAL * 30.4375),
