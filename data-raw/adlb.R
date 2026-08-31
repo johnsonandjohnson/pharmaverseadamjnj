78e3637cf2c4e48530ec5025d931d6b887d4aae1
sshanm46 <sshanm46@its.jnj.com>
2026-08-31 16:05:35 +0000

    linter updates

--- data-raw/adlb.R
+++ data-raw/adlb.R
@@ -638,10 +638,10 @@ gen_adlb <- function(seed = 123) {
     mutate(
       ANL02FL = dplyr::case_when(
                                  (grepl("Cycle", AVISIT) |
-           grepl("End Of Treatment", AVISIT) |
-           grepl("Baseline", AVISIT)) &
-                                   ANL01FL == "Y" & is.na(DTYPE) ~ "Y",
-                                TRUE ~ NA_character_)
+                                    grepl("End Of Treatment", AVISIT) |
+                                       grepl("Baseline", AVISIT)) &
+                                 ANL01FL == "Y" & is.na(DTYPE) ~ "Y",
+                                 TRUE ~ NA_character_)
     )
 
   # Apply admiral::restrict_derivation for ANL03FL
